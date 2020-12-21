e3e96548d651f29dfdbd17b54dc9072546e77fa2
Faiz Surani <faiz.surani@gmail.com>
2020-12-20 20:14:13 -0800

    Add affirmative action paper

---
+++ writing/affirmative-action.html.pm
@@ -0,0 +1,11 @@
+#lang pollen
+
+◊(define-meta title            "Affirmative Action Has Failed Us")
+◊; ◊(define-meta doc-publish-date "2019-12-13")
+◊(define-meta author           "Faiz Surani")
+◊(define-meta summary          "A deep dive into the failures of affirmative action — and what happens next.")
+◊(define-meta series           "series/law.html")
+◊(define-meta post-type        "writing.html")
+◊(define-meta pdf-url          "assets/affirmative-action.pdf")
+
+◊p{A deep dive into the failures of affirmative action — and what happens next.}
