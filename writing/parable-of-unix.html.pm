3f69002b16bde25a87105bc683aa9ed31e4a7724
Faiz Surani <faiz.surani@gmail.com>
2021-03-01 18:48:06 -0800

    Add parable of unix article

---
+++ writing/parable-of-unix.html.pm
@@ -0,0 +1,10 @@
+#lang pollen
+
+◊(define-meta title            "The Parable of Unix")
+◊(define-meta author           "Faiz Surani")
+◊(define-meta summary          "Unix's accidental path to world domination and what it can tell us about software copyright law.")
+◊(define-meta series           "series/law.html")
+◊(define-meta post-type        "writing.html")
+◊(define-meta pdf-url          "assets/parable-of-unix.pdf")
+
+◊p{Unix's accidental path to world domination and what it can tell us about software copyright law.}
