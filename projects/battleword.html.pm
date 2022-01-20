105066e12045cc1fcfcad459584376db1634665a
Faiz Surani <faiz.surani@gmail.com>
2022-01-20 15:52:11 -0800

    Add battleword and lexcaliber projects

---
+++ projects/battleword.html.pm
@@ -0,0 +1,13 @@
+
+#lang pollen
+
+◊(define-meta title            "BattleWord")
+◊(define-meta author           "Faiz Surani and Varun Iyer")
+◊(define-meta summary          "A 1v1 word game inspired by Wordle. Guess your opponent's word first to win!")
+◊(define-meta series           "series/software.html")
+◊(define-meta post-type        "projects.html")
+◊(define-meta website-url      "https://battleword.io")
+◊(define-meta github-url       "https://github.com/ProbablyFaiz/battleword")
+
+◊p{BattleWord is a 1v1 online word game inspired by Wordle.
+Players must guess their opponent's word first to win -- but their guesses reveal information about their own word too.}
---
+++ projects/lexcaliber.html.pm
@@ -0,0 +1,12 @@
+
+#lang pollen
+
+◊(define-meta title            "Lexcaliber")
+◊(define-meta author           "Faiz Surani and Varun Iyer")
+◊(define-meta summary          "A project to develop novel recommendation and analysis algorithms for legal research via a context-enhanced network approach.")
+◊(define-meta series           "series/software.html")
+◊(define-meta post-type        "projects.html")
+◊(define-meta github-url       "https://github.com/lexcaliber")
+
+◊p{Lexcaliber is an ongoing project to develop novel recommendation and analysis algorithms for legal research.}
+◊p{Our current research focuses include an empirical analysis of circuit splits in the federal judiciary, context-assisted network representation, and ◊hyperlink{https://github.com/lexcaliber/scotus_explorer}{a prototype web app} to demo the project's capabilities for legal researchers.}
