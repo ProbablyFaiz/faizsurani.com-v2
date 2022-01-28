e7c1236e2f48ae21575d5f5488c67ddf70c95ddd
Faiz Surani <faiz.surani@gmail.com>
2022-01-27 21:42:11 -0800

    Rename battleword to worduel

--- src/projects/worduel.html.pm
+++ src/projects/worduel.html.pm
@@ -1,13 +1,13 @@
 
 #lang pollen
 
-◊(define-meta title            "BattleWord")
+◊(define-meta title            "Worduel")
 ◊(define-meta author           "Faiz Surani and Varun Iyer")
 ◊(define-meta summary          "A 1v1 word game inspired by Wordle. Guess your opponent's word first to win!")
 ◊(define-meta series           "series/software.html")
 ◊(define-meta post-type        "projects.html")
 ◊(define-meta website-url      "https://battleword.io")
-◊(define-meta github-url       "https://github.com/ProbablyFaiz/battleword")
+◊(define-meta github-url       "https://github.com/ProbablyFaiz/worduel")
 
-◊p{BattleWord is a 1v1 online word game inspired by Wordle.
+◊p{Worduel is a 1v1 online word game inspired by Wordle.
 Players must guess their opponent's word first to win -- but their guesses reveal information about their own word too.}
