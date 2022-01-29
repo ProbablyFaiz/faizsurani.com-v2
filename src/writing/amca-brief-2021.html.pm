a44ebaf32bd4798ff71ac52eda469c24dbd41f37
Faiz Surani <faiz.surani@gmail.com>
2022-01-28 23:00:26 -0800

    Add moot court briefs to website

---
+++ src/writing/amca-brief-2021.html.pm
@@ -0,0 +1,10 @@
+#lang pollen
+
+◊(define-meta title            "Moot Court: 2021 National Championship Brief")
+◊(define-meta author           "Faiz Surani and Varun Iyer")
+◊(define-meta summary          "The winning petitioner brief in the American Moot Court Association's 2021 brief-writing competition")
+◊(define-meta series           "series/law.html")
+◊(define-meta post-type        "writing.html")
+◊(define-meta pdf-url          "assets/amca-brief-2021.pdf")
+
+◊p{The winning petitioner brief in the 2021 American Moot Court Association national brief-writing competition. The case problem for which this brief was written can be found ◊hyperlink["https://static1.squarespace.com/static/5b660749620b85c6c73e5e61/t/61267e37256051269526f76d/1629912632226/20-21+AMCA+Case+-+Final+-+1+Sep+20.pdf"]{here}.}
---
+++ src/writing/amca-brief-2022.html.pm
@@ -0,0 +1,10 @@
+#lang pollen
+
+◊(define-meta title            "Moot Court: 2022 National Championship Brief")
+◊(define-meta author           "Faiz Surani and Varun Iyer")
+◊(define-meta summary          "The winning respondent brief in the American Moot Court Association's 2022 brief-writing competition")
+◊(define-meta series           "series/law.html")
+◊(define-meta post-type        "writing.html")
+◊(define-meta pdf-url          "assets/amca-brief-2022.pdf")
+
+◊p{The winning respondent brief in the 2022 American Moot Court Association national brief-writing competition. The case problem for which this brief was written can be found ◊hyperlink["https://static1.squarespace.com/static/5b660749620b85c6c73e5e61/t/6123c2ad4025626ddd55ed69/1629733549483/2021-22+AMCA+Case_Revised_August+2021.pdf"]{here}.}
