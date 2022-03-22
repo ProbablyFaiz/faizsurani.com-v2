17466c5cd02a39c9e6e0f03895e95043a90a9bda
Faiz Surani <faiz.surani@gmail.com>
2022-03-22 21:32:03 +0100

    Change lexcaliber links

--- src/projects/lexcaliber.html.pm
+++ src/projects/lexcaliber.html.pm
@@ -6,7 +6,7 @@
 ◊(define-meta summary          "A project to develop novel recommendation and analysis algorithms for legal research via a context-enhanced network approach.")
 ◊(define-meta series           "series/software.html")
 ◊(define-meta post-type        "projects.html")
-◊(define-meta github-url       "https://github.com/lexcaliber")
+◊(define-meta github-url       "https://github.com/lexeme-dev/core")
 
 ◊p{Lexcaliber is an ongoing project to develop novel recommendation and analysis algorithms for legal research.}
-◊p{Our current research focuses include an empirical analysis of circuit splits in the federal judiciary, context-assisted network representation, and ◊hyperlink["https://github.com/lexcaliber/scotus_explorer"]{a prototype web app} to demo the project's capabilities for legal researchers.}
+◊p{Our current research focuses include an empirical analysis of circuit splits in the federal judiciary, context-assisted network representation, and ◊hyperlink["https://github.com/lexeme-dev/explorer"]{a prototype web app} to demo the project's capabilities for legal researchers.}
