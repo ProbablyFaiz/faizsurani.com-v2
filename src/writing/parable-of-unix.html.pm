e646d55791c8bab965fbac4339f2c0d1fa052d63
Faiz Surani <faiz.surani@gmail.com>
2022-04-27 18:16:02 +0200

    Switch unix paper to SSRN DOI link

--- src/writing/parable-of-unix.html.pm
+++ src/writing/parable-of-unix.html.pm
@@ -5,6 +5,6 @@
 ◊(define-meta summary          "Unix's accidental path to world domination and what it can tell us about software copyright law.")
 ◊(define-meta series           "series/law.html")
 ◊(define-meta post-type        "writing.html")
-◊(define-meta pdf-url          "assets/parable-of-unix.pdf")
+◊(define-meta direct-url       "http://dx.doi.org/10.2139/ssrn.4082083")
 
 ◊p{Unix's accidental path to world domination and what it can tell us about software copyright law.}
