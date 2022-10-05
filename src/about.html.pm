05934e60896cee575332ae4099c47861cb52f157
Faiz Surani <faiz.surani@gmail.com>
2022-10-05 15:51:19 -0700

    Rewrite bio

--- src/about.html.pm
+++ src/about.html.pm
@@ -2,10 +2,8 @@
 
 ◊(define-meta title            "About Me")
 
-Hi, I'm Faiz (pronounced ◊smallcaps{fez}). I'm a Computing major in the ◊hyperlink["https://ccs.ucsb.edu"]{College of Creative Studies} at UC Santa Barbara. I currently work as a Software Engineering Intern at Google on the Neural Semantic Parsing team.
+Hi, I'm Faiz (pronounced ◊smallcaps{fez}). I'm a Computing major in the ◊hyperlink["https://ccs.ucsb.edu"]{College of Creative Studies} at UC Santa Barbara. I currently work as a Software Engineering Intern at Google on the Neural Semantic Parsing team. I'm also a research assistant at ◊hyperlink["https://reglab.stanford.edu"]{Stanford RegLab}, where I work on a number of NLP efforts in the legal domain.
 
-In the field of computer science, my current interests include  natural language processing. I am an ◊hyperlink["https://free.law/2022/03/17/summarizing-important-cases"]{active contributor} to the Free Law Project's suite of open-source legal research tools. You can find more of my work scattered across this website and my ◊hyperlink["https://github.com/ProbablyFaiz"]{GitHub} account.
-
-Outside of computer science, I'm a dedicated effective altruist. Since 2020, I've been a member of and volunteer for ◊hyperlink["https://givingwhatwecan.org"]{Giving What We Can}, for which I work on various writing and data science projects. I'm also passionate about public policy and constitutional law. My recent writing in these areas includes articles on ◊hyperlink["https://dailynexus.com/2020-12-25/the-supreme-court-considers-reviving-state-sponsored-discrimination/"]{religious liberty}, ◊hyperlink["/assets/parable-of-unix.pdf"]{copyright}, and ◊hyperlink["/assets/affirmative-action.pdf"]{affirmative action} law. Additionally, I co-founded and led UCSB's award-winning ◊hyperlink["https://ucsbmootcourt.org"]{moot court team} and co-authored ◊hyperlink["https://ucsbmootcourt.org/stockton-v-lsr"]{ a case problem} centered around the Free Exercise Clause and the Americans with Disabilities Act.
+In the field of computer science, my current interests include natural language processing, functional programming, and network science. Outside of CS, I'm also interested in a number of areas within public policy and constitutional law. You can find some of my work in these areas on this website.
 
 If you have an interesting opportunity or a good book recommendation, I'd love to ◊hyperlink["mailto:faiz@faizsurani.com"]{hear from you.}
