169a0bea823bbd2b22ded15bb8d82ccaf409f82f
Faiz Surani <faiz.surani@gmail.com>
2022-10-18 14:58:06 -0700

    Add Lexeme Search project

---
+++ src/projects/search.html.pm
@@ -0,0 +1,10 @@
+#lang pollen
+
+◊(define-meta title            "Lexeme Search")
+◊(define-meta author           "Faiz Surani and Varun Iyer")
+◊(define-meta summary          "Semantic search for the U.S. Supreme Court's decisions")
+◊(define-meta series           "series/software.html")
+◊(define-meta post-type        "projects.html")
+◊(define-meta website-url      "https://lexeme.dev")
+
+◊p{Lexeme Search is a semantic search engine for the U.S. Supreme Court's decisions. It allows users to search for cases by the concepts they contain, rather than by the words they use. For example, try: "What are the elements of fair use?"}
